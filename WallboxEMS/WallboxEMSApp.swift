e0250b16db589ddf86aa07da2723d8dc8e7ce4db
Ruben Marin <remcadiz@gmail.com>
2022-09-24 15:12:14 +0200

    Initial commit

---
+++ WallboxEMS/WallboxEMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallboxEMSApp.swift
+//  WallboxEMS
+//
+//  Created by Ruben Exposito Marin on 24/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WallboxEMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
